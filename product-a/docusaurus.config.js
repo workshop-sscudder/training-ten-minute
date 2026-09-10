987130499d566971853a5b491bbecb8701a9f173
workshop-sscudder <SarigMoshe_Scudder@bmc.com>
2026-09-10 13:34:10 +0300

    docusaurus config file

--- product-a/docusaurus.config.js
+++ product-a/docusaurus.config.js
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://workspace-sscudder.github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/product-a/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'workspace-sscudder', // Usually your GitHub org/user name.
+  projectName: 'product-a', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
